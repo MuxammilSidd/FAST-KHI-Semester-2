94a8b92b1c19cae681b427c56e065cfd422f61f4
Muhammad Muzammil Siddiqui <memuzammilsiddiqui@gmail.com>
2024-04-28 22:00:00 +0500

    Home tasks

--- OOP (Theory)/Assignments/A3-23K2001/A3-Q1_23K2001.cpp
+++ OOP (Theory)/Assignments/A3-23K2001/A3-Q1_23K2001.cpp
@@ -1,8 +1,104 @@
 //23K2001 Muzammil Q1
 #include<iostream>
 using namespace std;
+class medicine{
+    protected:
+    string name,formula;
+    float price;
+    int mfDate,expDate;
+    
+    public:
+    medicine(){}
+    medicine(string name,string formula,float price,int mfDate,int expDate){
+        this->name=name;
+        this->formula=formula;
+        this->price=price;
+        this->mfDate=mfDate;
+        this->expDate=expDate;
+    }
+    void setName(string name){ this->name = name; }
+    void setFormula(string formula){ this->formula = formula; }
+    void setPrice(float price){ this->price = price; }
+    void setMdate(int mfDate){ this->mfDate = mfDate; }
+    void setEDate(int expDate){ this->expDate = expDate; }
 
+    string getName(){ return name; }
+    string getFormula(){ return formula; }
+    float getPrice(){ return price; }
+    int getMdate(){ return mfDate; }
+    int getEdate(){ return expDate; }
+
+    virtual void display(){
+        cout<<"\nName: "<<name<<endl;
+        cout<<"Formula: "<<formula<<endl;
+        cout<<"Price: "<<price<<endl;
+        cout<<"Manufacture: "<<mfDate<<endl;
+        cout<<"Expiry: "<<expDate<<endl;
+    }
+
+    bool operator ==(medicine &m1){
+        if(this->expDate/10000==m1.expDate/10000){
+            return 1;
+        }
+        else
+        return 0;
+    }
+};
+class Tablet : public medicine{
+    protected:
+    bool sucroseLevel;
+
+    public:
+    Tablet(string n,string f,float p,int mDate,int eDate,bool s_level):medicine(n,f,p,mDate,eDate),sucroseLevel(s_level){}
+    void display() override{
+        cout<<"\n\tTABLET";
+        medicine::display();
+        cout<<"Sucrose Level: "<<sucroseLevel<<endl;
+    }
+};
+class Capsule : public medicine{
+    protected:
+    int absPerc;
+
+    public:
+    Capsule(string n,string f,float p,int mDate,int eDate,int per):medicine(n,f,p,mDate,eDate),absPerc(per){}
+    void display() override{
+        cout<<"\n\tCAPSULE";
+        medicine::display();
+        cout<<"Absorption: "<<absPerc<<"%"<<endl;
+    }
+};
+class Syrup : public medicine{
+    protected:
+    int teaspoons;
+
+    public:
+    Syrup(string n,string f,float p,int mDate,int eDate,int teaspoon):medicine(n,f,p,mDate,eDate),teaspoons(teaspoon){}
+    void display() override{
+        cout<<"\n\tSYRUP";
+        medicine::display();
+        cout<<"Teaspoons: "<<teaspoons<<endl;
+    }
+};
+class pharmacist{
+    public:
+    void searchMedicine(){ }
+};
+class counter{
+    public:
+    void searchMedicine(){}
+    void updateRevenue(){}
+};
 int main(){
     cout<<"23K2001 - Muzammil\n\n"<<endl;
+
+    Tablet panadol("Panadol Fever Tablet","XXYA4210",50.75,20240101,20240530,1);
+    Capsule insulin("Insulin","XZMB3120",30.40,20240220,20240530,80);
+
+    if(panadol==insulin){
+        cout<<"Expiring in the same year!"<<endl;
+    }
+    else
+    cout<<"Expiry year different!"<<endl;
     return 0;
 }
--- OOP (Theory)/Assignments/A3-23K2001/A3-Q4_23K2001.cpp
+++ OOP (Theory)/Assignments/A3-23K2001/A3-Q4_23K2001.cpp
@@ -1,8 +1,62 @@
 //23K2001 Muzammil Q4
 #include<iostream>
+#include<cmath>
 using namespace std;
+class Flyable{
+    public:
+    virtual void takeoff()=0;
+    virtual void land()=0;
+    virtual float navigate(float latitude,float longitude,float altitude)=0;
+};
+class Scannable{
+    public:
+    virtual void scanArea(float radius)=0;
+};
+class Drone : public Flyable, public Scannable{
+    protected:
+    float longit,latit,speed,alt;
+
+    public:
+    void adjustAlt(float m){ alt = m; }
+    void setSpeed(float s=0){ speed = s; }
+    void setPos(float latitude,float longitude){
+        latit = latitude;
+        longit = longitude;
+    }
+};
+class Recondrone: public Drone{
+    protected:
+    int cameraResolution, maxTime;
+
+    public:
+    void takeoff(){ cout<<"\nDrone is taking off now!"<<endl; }
+    void land(){ cout<<"\nDrone is landing now!"<<endl; }
+    void scanArea(float radius){ cout<<"\nScanning for objects within "<<radius<<"m"<<endl; }
+    
+    float navigate(float latitude,float longitude,float altitude){
+        cout<<"\nDistance to target: ";
+        float d = sqrt(pow((latitude-latit),2)+pow((longitude-longit),2)+pow((altitude-alt),2));
+        cout<<d<<"m"<<endl;
+        
+        return d;
+    }
+    void timeRequired(float latitude,float longitude,float altitude){
+        float d = navigate(latitude,longitude,altitude);
+        cout<<"\nTime required to reach target at "<<speed<<" m/s"<<endl;
+        cout<<"& Altitude: "<<alt<<endl;
+        cout<<d/speed<<" sec"<<endl;
+    }   
+};
 
 int main(){
-    cout<<"23K2001 - Muzammil\n\n"<<endl;
+    cout<<"23K2001 - Muzammil"<<endl;
+    Recondrone fighterX;
+    fighterX.setPos(70,65);
+    fighterX.adjustAlt(80);
+    fighterX.setSpeed(5);
+    fighterX.navigate(90,115,97);
+    fighterX.timeRequired(90,115,97);
+    fighterX.scanArea(20);
+
     return 0;
 }
