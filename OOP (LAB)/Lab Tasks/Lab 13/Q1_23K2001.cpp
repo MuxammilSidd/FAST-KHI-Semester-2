aa966d1ca8cbd361498d19ccde6e9e0d0a752395
Muhammad Muzammil Siddiqui <memuzammilsiddiqui@gmail.com>
2024-05-06 20:05:03 +0500

    OOP Lab Tasks

--- OOP (LAB)/Lab Tasks/Lab 13/Q1_23K2001.cpp
+++ OOP (LAB)/Lab Tasks/Lab 13/Q1_23K2001.cpp
@@ -2,30 +2,25 @@
 #include<iostream>
 using namespace std;
 void throwException(){
-    try{
         cout<<"Checking for errors.."<<endl;
-        if(1)
         throw 2001;
-
-        cout<<"Proceeded successfully"<<endl;
-    }
-    catch(int e){ cout<<"Error "<<e<<": Could not proceed!"<<endl; }
 }
 void funcC(){ 
-    throwException();
-    cout<<"Inside function C"<<endl; 
+    try{ throwException(); }
+    catch(int er){ cout<<"Error code: "<<er<<" - Inside function C"<<endl; }
     }
 
 void funcB(){
-    funcC(); 
-    cout<<"Inside function B"<<endl; 
+    try{ funcC(); }
+    catch(int er){ cout<<"Error code: "<<er<<" - Inside function B"<<endl; }
     }
 void funcA(){ 
-    funcB();
-    cout<<"Inside function A"<<endl; 
+    try{ funcB(); }
+    catch(int er){ cout<<"Error code: "<<er<<" - Inside function A"<<endl; }
     }
 int main()
 {
+    cout<<"23K2001 - Muzammil\n"<<endl;
 	funcA();
 	return 0;
 }
--- OOP (LAB)/Lab Tasks/Lab 13/Q2_23K2001.cpp
+++ OOP (LAB)/Lab Tasks/Lab 13/Q2_23K2001.cpp
@@ -1,19 +1,38 @@
 //23k2001  Q2
 #include<iostream>
 using namespace std;
-void parseData(){
-
+void parseData(string name){
+    if(name==""){string error = "Name cannot be empty!";
+    throw error;
+    }
+    else
+    cout<<"Name: "<<name<<" sent to processing!"<<endl;
 
 }
-void processData(){
+void processData(string name){
     try{
-
-
+        parseData(name);
+        cout<<"processing successful!"<<endl;
     }
-    catch(string e){ cout<<"Parsing error 101: "<<e<<endl; }
+    catch(string e){ 
+        cout<<"Parsing error 101: "<<e<<endl;
+        string error2 = "processing failed!";
+        throw error2;
+         }
 }
 int main()
 {
-	
+	try{ processData(""); }
+    catch(string e){ 
+        cout<<e<<endl;
+        cout<<"Please enter name correctly!"<<endl; }
+
+    cout<<endl<<endl;
+    
+    try{ processData("Muzammil"); }
+    catch(string e){ 
+        cout<<e<<endl;
+        cout<<"Please enter name correctly!"<<endl; }
+
 	return 0;
 }
